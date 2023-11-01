01b11105b1216ada78fd392a2f07c39027e58928
srshivak <srshivak@LIN-PF1TMLNY>
2023-11-01 22:30:58 +0530

    Add project files.

---
+++ Day15_Linq3/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Diagnostics.Tracing;
+using Day15_Linq3;
+
+Console.WriteLine("Enter SizeOfArr");
+int SizeOfArr = Convert.ToInt32(Console.ReadLine());
+string[] arr = new string[SizeOfArr];
+
+for (int i = 0; i < SizeOfArr; i++)
+{
+    Console.WriteLine("enter a word");
+    arr[i] = Console.ReadLine();
+}
+
+/*Console.WriteLine("arr is");
+ 
+foreach (string str in arr)
+ 
+{
+    Console.WriteLine(str);
+}*/
+
+Console.WriteLine("removing somebody and nobody");
+var result = arr.Where(e => e != "nobody" && e != "somebody");
+foreach (var i in result)
+{
+    Console.WriteLine(i);
+}
+
+var result2 = from e in arr
+              where e != "nobody" && e != "somebody"
+              select e;
+foreach (var i in result2)
+{
+    Console.WriteLine(i);
+}
---
+++ Day15_Linq3/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day15_Linq3
+{
+    internal class Student
+    {
+        public int StudentId { get; set; }
+        public string StudName { get; set; }
+
+        public void
+    }
+}
